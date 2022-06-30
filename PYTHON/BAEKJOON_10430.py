4c062fb7ced7ed6522e34a2d3bb73607b5d4dfb5
sangho527 <sy980527@naver.com>
2022-06-30 20:42:51 +0900

    나머지

--- PYTHON/BAEKJOON_10430.py
+++ PYTHON/BAEKJOON_10430.py
@@ -7,5 +7,5 @@ A, B, C = map(int, input().split())
 #print(((A%C) + (B%C))%C)
 #print((A*B)%C)
 #print(((A%C)*(B%C))%C)
-
+ 
 print((A+B)%C, ((A%C) + (B%C))%C, (A*B)%C, ((A%C)*(B%C))%C ,sep= '\n') # sep 파라미터를 사용해 출력값이 여러개인 경우 각 출력값 사이에 삽입할 문자를 지정
