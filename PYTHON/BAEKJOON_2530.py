08c36cbabf33a2a25b9283e9f81f463d27827d67
sangho527 <sy980527@naver.com>
2022-07-05 16:22:39 +0900

    인공지능 시계

--- PYTHON/BAEKJOON_2530.py
+++ PYTHON/BAEKJOON_2530.py
@@ -7,7 +7,7 @@ hour1 = (hour + (minute+((sec+add)//60))//60)%24
 minute1 = (minute+((sec+add)//60))%60
 sec1 = (sec+add)%60
 
-print(hour1, minute1, sec1)
+print(hour1, minute1, sec1) 
 
 
 # ((sec+add)%60) = 몫은 101 나머지는 0
