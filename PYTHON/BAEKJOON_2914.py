e9886a642d28404cc2b1a1dddd50ae28802c8841
sangho527 <sy980527@naver.com>
2022-07-06 22:59:37 +0900

    저작권문제(평균값 구하기)

---
+++ PYTHON/BAEKJOON_2914.py
@@ -0,0 +1,4 @@
+#저작권 문제 평균값 구하기
+
+a,b = map(int, input().split())
+print(a*(b-1)+1)
--- 연습.py
+++ 연습.py
@@ -1,7 +1,7 @@
 hour, minute = map(int, input().split())
 add = int(input())
 
-hour1 = ((hour)+(minute+add)//60)%24
+hour1 = (hour+(minute+add)//60)%24
 minute1 = (minute+add)%60
 
 print(hour1, minute1)
