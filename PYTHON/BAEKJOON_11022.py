8bae7ad8f666a9356f3ee760327c5f73e3c95f2b
sangho527 <sy980527@naver.com>
2022-08-05 14:09:37 +0900

    A+B - 8

--- PYTHON/BAEKJOON_11022.py
+++ PYTHON/BAEKJOON_11022.py
@@ -1,5 +1,7 @@
-T = int(input())
-for i in range(1, T+1):
-    a,b = map(int, input().split())
-    #print("Case #" + str(i) + ":", a, "+", b, "=", a+b) // "case #" 출력, str(i), 문자열 반복문출력
-    print(f'Case #{i}: {a} + {b} = {a+b}') 
+t = int(input())
+
+for X in range(1, t+1):
+
+    a, b = map(int, input().split())
+    c = a+b
+    print(f'Case #{X}: {a} + {b} = {c}') # fstring 사용
