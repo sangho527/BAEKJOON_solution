1290a876ee73a6a1e87ff90002e85c86f2c05aaf
sangho527 <sy980527@naver.com>
2022-08-12 12:15:48 +0900

    문자열반복

--- PYTHON/BAEKJOON_2675.py
+++ PYTHON/BAEKJOON_2675.py
@@ -1,7 +1,7 @@
 t = int(input())
 for i in range(t):
-    num, s = input().split()
+    num, s = input().split() #문자열을 입력받고 줄 바꿈
     text = ''
-    for i in s:
+    for i in s: #반복문
         text += int(num) * i
     print(text)
