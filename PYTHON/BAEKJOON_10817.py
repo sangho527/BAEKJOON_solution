e005dd229defed8888df03feb9b4b62ccc7e1989
sangho527 <sy980527@naver.com>
2022-08-16 02:36:40 +0900

    세 수

--- PYTHON/BAEKJOON_10817.py
+++ PYTHON/BAEKJOON_10817.py
@@ -1,3 +1,22 @@
 num_list = list(map(int, input().split()))
 num_list.sort()
 print(num_list[1])
+
+"""a, b, c = map(int, input().split(' '))
+
+if(a >= b):
+    if(a >= c):
+        if(b >= c):
+            print(b)
+        else:
+            print(c)
+    else:
+        print(a)
+else:
+    if(b >= c):
+        if(a >= c):
+            print(a)
+        else:
+            print(c)
+    else:
+        print(b)""" #if문 사용
