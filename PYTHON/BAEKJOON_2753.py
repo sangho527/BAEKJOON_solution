794e207e5a56c8af6c0f3ca26327dbe4c156649a
sangho527 <sy980527@naver.com>
2022-08-24 19:50:02 +0900

    윤년

--- PYTHON/BAEKJOON_2753.py
+++ PYTHON/BAEKJOON_2753.py
@@ -1,5 +1,5 @@
 a = int(input())
-if (a % 4 == 0 and a % 100 != 0) or a % 400 == 0:
-    print(1)
+if (a % 4 == 0 and a % 100 != 0) or a % 400 == 0: #윤년의 코드
+    print(1) #윤년이면
 else:
-    print(0)
+    print(0) #윤년이 아니면
