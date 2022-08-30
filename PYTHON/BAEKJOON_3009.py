c779039bcf93fe87403feaa7e0e4a7f11e7445c8
sangho527 <sy980527@naver.com>
2022-08-30 11:37:32 +0900

    네 번째 점

--- PYTHON/BAEKJOON_3009.py
+++ PYTHON/BAEKJOON_3009.py
@@ -2,10 +2,10 @@ x_ = []
 y_ = []
 for i in range(3):
         x, y = map(int, input().split())
-        x_.append(x)
+        x_.append(x) #append 덧붙이다
         y_.append(y)
 for i in range(3):
-        if x_.count(x_[i]) == 1:
+        if x_.count(x_[i]) == 1: #요소의 개수를 세서 개수가 하나인 것을 출력
                 x = x_[i]
         if y_.count(y_[i]) == 1:
                 y = y_[i]
