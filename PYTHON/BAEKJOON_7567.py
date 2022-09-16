a98b6a3f2f9bfe802ded346680821d49a5034cd1
sangho527 <sy980527@naver.com>
2022-09-16 18:28:21 +0900

    그릇

--- PYTHON/BAEKJOON_7567.py
+++ PYTHON/BAEKJOON_7567.py
@@ -1,7 +1,7 @@
-dish = list(str(input()))
+dish = list(str(input())) # ㄹㅣ스트를에 넣어서 저장
 answer = 0
 
-for i in range(len(dish)):
+for i in range(len(dish)): 
     if i == 0:
         answer += 10
     elif dish[i] == dish[i-1]:
